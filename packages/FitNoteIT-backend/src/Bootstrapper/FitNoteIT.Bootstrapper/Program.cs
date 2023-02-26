bc8da1f333a69a4fcab58917bc7b3a7e3904a426
Kamil Tomiak <52603787+Resterr@users.noreply.github.com>
2023-02-26 19:10:08 +0100

    Added CORS policy

--- packages/FitNoteIT-backend/src/Bootstrapper/FitNoteIT.Bootstrapper/Program.cs
+++ packages/FitNoteIT-backend/src/Bootstrapper/FitNoteIT.Bootstrapper/Program.cs
@@ -3,6 +3,21 @@ using FitNoteIT.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false)
+        .Build();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder =>
+        {
+            builder.WithOrigins(config["Cors:AllowedOrigins"].Split(","))
+                   .WithMethods(config["Cors:AllowedMethods"].Split(","))
+                   .WithHeaders(config["Cors:AllowedHeaders"].Split(","));
+        });
+});
+
 builder.Services.AddSharedFramework();
 builder.Services.AddUsersModule(builder.Configuration);
 
@@ -10,6 +25,8 @@ var app = builder.Build();
 
 app.UseSharedFramework();
 app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+
 app.MapGet("/", ctx => ctx.Response.WriteAsync("FitNoteIT API is ok"));
 app.RegisterUsersModuleRequests();
 
