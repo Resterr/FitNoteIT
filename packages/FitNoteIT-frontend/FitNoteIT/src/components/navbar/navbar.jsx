4d15b581250a455499caa13eeda6c76bcac7755c
Michalbeblocinski <mboss211xd@interia.pl>
2023-02-26 22:25:30 +0100

    3page

--- packages/FitNoteIT-frontend/FitNoteIT/src/components/navbar/navbar.jsx
+++ packages/FitNoteIT-frontend/FitNoteIT/src/components/navbar/navbar.jsx
@@ -3,8 +3,12 @@ import { Outlet } from "react-router-dom";
 import SwipeableTemporaryDrawer from "../menu/menu";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import logo from "../../assets/Logo.png"
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <Fragment>
       <div className="navbar">
@@ -16,26 +20,44 @@ function Navbar() {
             <Link to='/'>
               <p>Rekordy</p>
             </Link>
-           </div> 
-           <div className="left2">
+          </div>
+          <div className="left2">
             <Link to='/'>
               <p>Historia</p>
             </Link>
           </div>
           <div className="center">
-            <Link to='/'>
+            {location.pathname === '/' ? (
+              <Link to='/'>
               <button className="navbar__start-button">Zacznij trening!</button>
             </Link>
+            ) : (
+              <Link to='/'>
+              <img className="navabar__logo" src={logo} alt="logo"/>
+            </Link>
+            )}
+            
           </div>
           <div className="right">
             <Link to='/'>
               <p>stwórz plan</p>
             </Link>
-            </div>
-            <div className="right2">
-            <Link to='/'>
-              <button className="navbar__logout-button">Wyloguj</button>
+          </div>
+          <div className="right2">
+          {location.pathname === '/' ? (
+            <Link to='/login'>
+              <button className="navbar__logout-button">Zaloguj</button>
             </Link>
+            ) : location.pathname === '/login' ? (
+              <Link to='/register'>
+              <button className="navbar__logout-button">Zarejestruj</button>
+            </Link>
+            ) : (
+              <Link to='/login'>
+              <button className="navbar__logout-button">Zaloguj</button>
+            </Link>
+            )}
+           
           </div>
         </div>
       </div>
