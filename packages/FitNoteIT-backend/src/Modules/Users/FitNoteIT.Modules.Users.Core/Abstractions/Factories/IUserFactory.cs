e1f0fcbbb0f5386b855e52940a16454828aed6e9
Kamil Tomiak <52603787+Resterr@users.noreply.github.com>
2023-03-05 20:11:29 +0100

    Changed accessibility of IUserFactory to public

--- packages/FitNoteIT-backend/src/Modules/Users/FitNoteIT.Modules.Users.Core/Abstractions/Factories/IUserFactory.cs
+++ packages/FitNoteIT-backend/src/Modules/Users/FitNoteIT.Modules.Users.Core/Abstractions/Factories/IUserFactory.cs
@@ -1,7 +1,7 @@
 ﻿using FitNoteIT.Modules.Users.Core.Entities;
 
 namespace FitNoteIT.Modules.Users.Core.Abstractions.Factories;
-internal interface IUserFactory
+public interface IUserFactory
 {
     User Create(Guid id, string email, string passwordHash, string userName, DateTime createdAt, Role userRole);
 }
