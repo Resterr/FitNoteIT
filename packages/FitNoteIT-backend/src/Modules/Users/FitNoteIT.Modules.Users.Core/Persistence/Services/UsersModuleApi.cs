ad2c4016d3221eaae58496b20c254a57d1907e02
Kamil Tomiak <52603787+Resterr@users.noreply.github.com>
2023-03-09 12:41:19 +0100

    Added UsersModuleApi

---
+++ packages/FitNoteIT-backend/src/Modules/Users/FitNoteIT.Modules.Users.Core/Persistence/Services/UsersModuleApi.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using FitNoteIT.Modules.Users.Core.Abstractions.Repositories;
+using FitNoteIT.Modules.Users.Shared;
+using FitNoteIT.Modules.Users.Shared.DTO;
+using FitNoteIT.Shared.Exceptions;
+
+namespace FitNoteIT.Modules.Users.Core.Persistence.Services;
+internal sealed class UsersModuleApi : IUsersModuleApi
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UsersModuleApi(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UserDto> GetUserAsync(Guid userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId);
+
+        if (user == null) throw new NotFoundException("User not found");
+
+        var result = _mapper.Map<UserDto>(user);
+
+        return result;
+    }
+
+    public async Task<UserDto> GetUserAsync(string userName)
+    {
+        var user = await _userRepository.GetByUserName(userName);
+
+        if (user == null) throw new NotFoundException("User not found");
+
+        var result = _mapper.Map<UserDto>(user);
+
+        return result;
+    }
+}
---
+++ packages/FitNoteIT-backend/src/Modules/Users/FitNoteIT.Modules.Users.Shared/IUsersModuleApi.cs
@@ -0,0 +1,8 @@
+﻿using FitNoteIT.Modules.Users.Shared.DTO;
+
+namespace FitNoteIT.Modules.Users.Shared;
+public interface IUsersModuleApi
+{
+    Task<UserDto> GetUserAsync(Guid userId);
+    Task<UserDto> GetUserAsync(string userName);
+}
