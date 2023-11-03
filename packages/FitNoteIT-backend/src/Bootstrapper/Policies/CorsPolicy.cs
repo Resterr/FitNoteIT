e716a4c316a45814a9fdb0396073bc4a287f1940
Kamil Tomiak <52603787+Resterr@users.noreply.github.com>
2023-11-03 15:14:27 +0100

    Update CorsPolicy.cs

--- packages/FitNoteIT-backend/src/Bootstrapper/Policies/CorsPolicy.cs
+++ packages/FitNoteIT-backend/src/Bootstrapper/Policies/CorsPolicy.cs
@@ -11,11 +11,11 @@ internal static class CorsPolicy
 		{
 			options.AddPolicy("CorsPolicy", builder =>
 			{
-				builder.WithOrigins(config["Cors:AllowedOrigins"]
+				builder.WithOrigins(config["Cors:AllowedOrigins"]!
 						.Split(","))
-					.WithMethods(config["Cors:AllowedMethods"]
+					.WithMethods(config["Cors:AllowedMethods"]!
 						.Split(","))
-					.WithHeaders(config["Cors:AllowedHeaders"]
+					.WithHeaders(config["Cors:AllowedHeaders"]!
 						.Split(","));
 			});
 		});
